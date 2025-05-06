222f670992ee826f38e7edaa30882e7ae2d9bafc
MichalAniol258 <146379528+MichalAniol258@users.noreply.github.com>
2025-05-06 14:46:49 +0200

    Update header.jsx

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/navigation';
 import { useSearchParams } from 'next/navigation'
 import Link from "next/link";
 import { Tooltip } from "@heroui/react";
+import {useMediaQuery} from "@mantine/hooks";
 
 
 const SEARCH_ANIME = gql`
@@ -169,7 +170,7 @@ export default function Header({ handleChangeFocus }) {
     const [showResults, setResults] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const isAdult = false;
-
+    const isClass = useMediaQuery("(min-width: 1024px)");
     const searchParams = useSearchParams()
     const search = searchParams.get("search")
     const type2 = searchParams.get("type")
@@ -1407,7 +1408,7 @@ export default function Header({ handleChangeFocus }) {
 
                     >
 
-                        <div className={`filter filter-select  ${focus ? "hiddenChuj" : "dzialac"}`}>
+                        {isClass && <div className={`filter filter-select`}>
                             <div className="filterName">Search</div>
                             <div className="search-wrap2">
                                 <svg data-v-84c4e64c="" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="ikonka chujec"><path data-v-84c4e64c="" className="ikonka" fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>
@@ -1415,7 +1416,7 @@ export default function Header({ handleChangeFocus }) {
                                        onChange={handleChange} type="search" placeholder="Search" autoComplete="off" className="filterSearch" />
                                 <svg onClick={() => setQueryClear()} data-v-17620a08="" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" className="ikonkaX ikonkaFilter"><path data-v-17620a08="" fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
                             </div>
-                        </div>
+                        </div>}
 
                         <Dropdown className="dropDown"
                         >
@@ -1772,7 +1773,7 @@ export default function Header({ handleChangeFocus }) {
 
 
             {showResults && query && data?.Page?.media?.length > 0 && (
-                <motion.section className={`sMain ${focus ? "cos123" : ""}`}
+                <motion.section className={`sMain ${isClass && pathname === "/" ? "cos123" : ""}`}
                                 animate={focus || pathname.includes("/Browse") ? "showSection" : "hidSection"}
                                 initial={false}
                                 variants={variants}
